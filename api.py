905a4c3c2f7df3d978a944bf8b285803483be4b7
fernando peguero <fernandoapeguero@gmail.com>
2021-01-19 14:34:54 -0500

    added port environment variable to the server

--- api.py
+++ api.py
@@ -172,6 +172,6 @@ def auth_error(error):
     }), error.status_code
 
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='127.0.0.1' , port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0' , port=5000)
